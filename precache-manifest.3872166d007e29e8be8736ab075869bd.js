8f0d631b9d995dcded0c53c8a28b90f84f3417a1
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-27 15:20:31 +0530

    Updates

---
+++ precache-manifest.3872166d007e29e8be8736ab075869bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33c9b55f0e58bbc977268a9d7ec94aa",
+    "url": "/TODOApp/index.html"
+  },
+  {
+    "revision": "dcb800cc46bb4ca2c371",
+    "url": "/TODOApp/static/css/main.ae2c05e7.chunk.css"
+  },
+  {
+    "revision": "2e5912b7e41d7d4ea7d0",
+    "url": "/TODOApp/static/js/2.c13f0bb5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TODOApp/static/js/2.c13f0bb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcb800cc46bb4ca2c371",
+    "url": "/TODOApp/static/js/main.50379bdc.chunk.js"
+  },
+  {
+    "revision": "663e00a514da8df60c4d",
+    "url": "/TODOApp/static/js/runtime-main.cee2fba9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TODOApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
